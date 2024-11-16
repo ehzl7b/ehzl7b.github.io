4bca9a4ed0d743b05790c1d215a6aeedcd62ce9d
ehzl7b <ehzl7b@gmail.com>
2024-11-16 12:16:56 +0900

    f

--- _src/globals.js
+++ _src/globals.js
@@ -2,9 +2,9 @@ export default global = {
   site: {
     title: "어즐 블로그",  
     cats: [
-      {cat: "/page/aa", permalink: "/cat/aa/", title: "AA 카테고리"},
-      {cat: "/page/bb", permalink: "/cat/bb/", title: "BB 카테고리"},
-      {cat: "/page/cc", permalink: "/cat/cc/", title: "CC 카테고리"},
+      {cat: "/page/aa", permalink: "/cat/aa/", icon: "O", title: "AA 카테고리"},
+      {cat: "/page/bb", permalink: "/cat/bb/", icon: "X", title: "BB 카테고리"},
+      {cat: "/page/cc", permalink: "/cat/cc/", icon: "Z", title: "CC 카테고리"},
     ],
   },
   // default
--- index.js
+++ index.js
@@ -26,6 +26,12 @@ function render(file, vars) {
   }
 }
 
+// 렌더링 결과 저장 함수
+function save(permalink, content) {
+  let path = `${$_site}${permalink.endsWith("/") ? permalink + "index.html" : permalink }`;
+  fs.outputFileSync(path, content);
+}
+
 const $_src = "./_src";
 const $_site = "./_site";
 const pagesGlob = fg.globSync(`${$_src}/**/*.{md,liquid}`, {ignore: `${$_src}/_layout/**/*`});
@@ -48,11 +54,33 @@ for (let file of pagesGlob) {
 // }
 
 // 카테고리 페이지 생성
-for (let {cat, permalink, title} of global.site.cats) {
-  if (cat in pagesMap) {
-    console.log(cat, permalink, title);
-  }
-}
+// for (let {cat, permalink, icon, title} of global.site.cats) {
+//   if (cat in pagesMap) {
+//     // console.log(cat, permalink, title);
+//     let pages = pagesMap[cat];
+//     let {vars, content} = render(`${$_src}/_layout/catpage.liquid`, {permalink, icon, title, pages});
+// 
+//     save(vars.permalink, content);
+//   }
+// }
 
+// console.log(pagesMap);
 
 // sitemap.xml 생성
+{
+  let allpages = [];
+  for (let [_, pages] of Object.entries(pagesMap)) {
+    allpages = allpages.concat(pages);
+  }
+  // console.log(allpages);
+  let {vars, content} = render(`${$_src}/_layout/sitemap.liquid`, {pages: allpages});
+  save(vars.permalink, content);
+
+  /**
+   * 
+   * 
+   * 파일 정보에 updated 필요...
+   * 
+   * 
+   */
+}
